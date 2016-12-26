a7ab76fa0b420f8f2434b65a989682ac14f28a44
robbie.luijben <diggingforfire@gmail.com>
2016-12-26 20:32:14 +0100

    Boilerplate

---
+++ src/Microblog.Web/gulpfile.js
@@ -0,0 +1,35 @@
+﻿var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	minify = require('gulp-cssmin'),
+	uglify = require('gulp-uglify');
+
+var paths = {
+	webroot: "./wwwroot/"
+};
+
+paths.jsDest = paths.webroot + "js";
+paths.cssDest = paths.webroot + "css";
+paths.fontDest = paths.webroot + "fonts";
+
+gulp.task("min:js", function() {
+	
+});
+
+gulp.task("copy:js", function () {
+
+});
+
+gulp.task("min:css", function () {
+
+});
+
+gulp.task("copy:css", function () {
+
+});
+
+gulp.task("copy:fonts", function () {
+
+});
+
+gulp.task("min", ["min:js", "min:css"]);
+gulp.task("copy", ["copy:js", "copy:css", "copy:fonts"]);
