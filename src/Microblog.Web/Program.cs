a0f16505e5313d9811bba92d2e45c4eda10abc17
Robbie Luijben <diggingforfire@gmail.com>
2017-01-01 19:29:41 +0100

    upgrade to .net core 1.1.0 + hosting in separate file

--- src/Microblog.Web/Program.cs
+++ src/Microblog.Web/Program.cs
@@ -1,20 +1,26 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Microblog.Web
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var config = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("hosting.json", optional: true)
+				.Build();
 
-            host.Run();
-        }
-    }
+			var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseConfiguration(config)
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				.UseStartup<Startup>()
+				.Build();
+
+			host.Run();
+		}
+	}
 }
